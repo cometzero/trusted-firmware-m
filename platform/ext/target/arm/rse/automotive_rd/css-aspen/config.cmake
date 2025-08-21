b676c04df54797a474bd5e160b70d138ecb0749a
Debbie Horsfall <debbie.horsfall@arm.com>
2025-08-21 16:09:09 +0100

    css-aspen: Increase size of AP region for MHU Pointer-access protocol
    This relates to an increase in the RSE carveout region size
    increase from 16KB (4x4KB pages) to 256KB (64x4KB pages).
    
    Upstream-Status: Pending
    Signed-off-by: Debbie Horsfall <debbie.horsfall@arm.com>
    Signed-off-by: Michael Zhao <michael.zhao2@arm.com>
    Signed-off-by: Ambroise Vincent <ambroise.vincent@arm.com>

--- platform/ext/target/arm/rse/automotive_rd/css-aspen/config.cmake
+++ platform/ext/target/arm/rse/automotive_rd/css-aspen/config.cmake
@@ -35,6 +35,7 @@ set(TFM_PARTITION_INTERNAL_TRUSTED_STORAGE ON    CACHE BOOL    "Enable Internal 
 set(TFM_PARTITION_MEASURED_BOOT         ON       CACHE BOOL    "Enable Measured Boot partition")
 set(TFM_PARTITION_PROTECTED_STORAGE     ON       CACHE BOOL    "Enable Protected Storage partition")
 set(TFM_PARTITION_SCMI_COMMS            ON       CACHE BOOL    "Enable SCMI Comms partition")
+set(SFCP_ATU_REGION_SIZE                0x20000  CACHE STRING  "The size of the carveout region")
 set(SFCP_NUMBER_NODES                   3        CACHE STRING  "Amount of nodes in the SFCP system, by default equal to number of RSEs")
 
 # Once all cache options are set, set common options as fallback
